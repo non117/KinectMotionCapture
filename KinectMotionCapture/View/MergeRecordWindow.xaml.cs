bd08031487e6b742826c18f717e0fc2797e8c1b1
non <mail@non117.com>
2014-11-10 13:39:29 +0900

    ディレクトリ構成を整理した

---
+++ KinectMotionCapture/View/MergeRecordWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+using System.IO;
+using MsgPack.Serialization;
+
+namespace KinectMotionCapture
+{
+    /// <summary>
+    /// MergeRecordWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MergeRecordWindow : Window
+    {
+
+
+        public MergeRecordWindow()
+        {
+            //List<string> datadir = new List<string>() { @"C:\Users\non\Desktop\data\1015_kinect4\mite1" };
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+        }
+    }
+}
