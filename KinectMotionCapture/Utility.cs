b69da4c43295c051b95487301dfcf860d555469e
non <mail@non117.com>
2014-11-17 14:21:10 +0900

    DPマッチング実装したけどバグってるっぽい

--- KinectMotionCapture/Utility.cs
+++ KinectMotionCapture/Utility.cs
@@ -195,5 +195,88 @@ namespace KinectMotionCapture
             }
             return newJoints;
         }
+
+        public class Point
+        {
+            public int x, y;
+            public Point(){ this.x = 0; this.y = 0; }
+            public Point(int x, int y){ this.x = x; this.y = y; }
+        }
+
+        public class Node
+        {
+            public float cost;
+            public Point prev;
+            public Point current;
+            public Node() { this.cost = float.MaxValue; }
+            public Node(float cost, Point prev, Point cur)
+            {
+                this.cost = cost; this.prev = prev; this.current = cur;
+            }
+        }
+
+        public static Tuple<float, int[]> DPmatching<Type>(List<Type> modelSeq, List<Type> targetSeq, Func<Type, Type, float> costFunc)
+        {
+            int m = modelSeq.Count();
+            int n = targetSeq.Count();
+            int x, y;
+            Node[,] pathMatrix = new Node[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    pathMatrix[i, j] = new Node();
+                }
+            }
+
+            Node point = new Node(costFunc(modelSeq[0], targetSeq[0]), new Point(-1, -1), new Point(0, 0));
+            List<Node> priorityList = new List<Node>();
+            priorityList.Add(point);
+
+            int[] dirX = { 0, 1, 1 };
+            int[] dirY = { 1, 1, 0 };
+
+            while (priorityList.Count > 0)
+            {
+                Node curNode = priorityList[0];
+                priorityList.RemoveAt(0);
+                
+                if (pathMatrix[curNode.current.x, curNode.current.y].cost < curNode.cost){ continue; }
+                if (curNode.current.x == m - 1 && curNode.current.y == n - 1) { break; }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    int nX = curNode.current.x + dirX[i];
+                    int nY = curNode.current.y + dirY[i];
+                    float addCost = costFunc(modelSeq[nX], targetSeq[nY]);
+                    if (nX < m && nY < n && pathMatrix[nX, nY].cost > curNode.cost + addCost)
+                    {
+                        pathMatrix[nX, nY].cost = curNode.cost + addCost;
+                        pathMatrix[nX, nY].prev = curNode.current;
+                        priorityList.Add(new Node(pathMatrix[nX, nY].cost, curNode.current, new Point(nX, nY)));
+                    }                    
+                }
+                priorityList = priorityList.OrderBy(node => node.cost).ToList();
+            }
+
+            List<Point> minPath = new List<Point>();
+            x = m - 1;
+            y = n - 1;
+            while (x != -1)
+            {
+                Node node = pathMatrix[x, y];
+                minPath.Add(new Point(x, y));
+                x = node.prev.x;
+                y = node.prev.y;
+            }
+            int[] res = new int[m];
+            foreach (Point p in minPath)
+            {
+                int i = p.x;
+                int j = p.y;
+                res[i] = j;
+            }
+            return new Tuple<float, int[]>(pathMatrix[m - 1, n - 1].cost / minPath.Count, res);
+        }
     }
 }
