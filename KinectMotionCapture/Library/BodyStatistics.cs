1c7f2d1d6427529f498ef8951cc7250e9524a02f
non <mail@non117.com>
2014-12-24 18:58:15 +0900

    ちょっと実装したｑ
    おなかへり

--- KinectMotionCapture/Library/BodyStatistics.cs
+++ KinectMotionCapture/Library/BodyStatistics.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Kinect;
+using OpenCvSharp;
+
 namespace KinectMotionCapture
 {
-    class BodyStatistics
+    public class BodyStatistics
     {
+        private List<Tuple<JointType, JointType>> bones;
+        private Dictionary<Tuple<JointType, JointType>, double> boneLengthSqLog;
+
+        public void StoreBones(Dictionary<JointType, Joint> joints)
+        {
+            Dictionary<JointType, Joint> validJoints = Utility.GetValidJoints(joints);
+            Joint firstJoint, secondJoint;
+            Tuple<JointType, JointType> boneKey;
+            foreach (var bone in bones)
+            {
+                if (validJoints.TryGetValue(bone.Item1, out firstJoint) && validJoints.TryGetValue(bone.Item2, out secondJoint))
+                {
+                    double lengthSq = CvEx.GetDistanceSq(firstJoint.Position.ToCvPoint3D(), secondJoint.Position.ToCvPoint3D());
+                    
+                }
+            }
+        }
+
+        public BodyStatistics()
+        {
+            this.bones = Utility.GetBones();
+            this.boneLengthSqLog = new Dictionary<Tuple<JointType, JointType>, double>();
+        }
     }
 }
