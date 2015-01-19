81a1b43ce3e58c5592072b01e7b5164641aa6105
non <mail@non117.com>
2015-01-19 15:27:11 +0900

    足を伸ばすやつをつくった
    もよもよ

--- KinectMotionCapture/Library/JointCorrection.cs
+++ KinectMotionCapture/Library/JointCorrection.cs
@@ -9,6 +9,8 @@ using OpenCvSharp;
 
 namespace KinectMotionCapture
 {
+    using Bone = Tuple<JointType, JointType>;
+
     class JointCorrection
     {
         /// <summary>
@@ -38,9 +40,29 @@ namespace KinectMotionCapture
             return angle;
         }
 
-        private void NormalizeLegJoints(SerializableBody body)
+        /// <summary>
+        /// 足の長さを統計情報をもとに正規化する
+        /// </summary>
+        /// <param name="body"></param>
+        private void NormalizeLegJoints(Dictionary<JointType, Joint> joints, Dictionary<Bone, BoneStatistics> boneStatistics)
         {
             List<Tuple<JointType, JointType>> legBones = Utility.GetLegBones();
+            foreach (Bone bone in legBones)
+            {
+                Joint joint1 = joints[bone.Item1];
+                Joint joint2 = joints[bone.Item2];
+                // 1の骨は動かさない. SpineBaseから順番に修正されることは保証されている.
+                double medianLength = Math.Sqrt(boneStatistics[bone].medianLengthSq);
+                CvPoint3D64f normalizedVector = CvEx.Normalize(joint1.Position.ToCvPoint3D() - joint2.Position.ToCvPoint3D());
+                CvPoint3D32f expandedVector = (CvPoint3D32f)(normalizedVector * medianLength);
+                joint2.Position = (joint1.Position.ToCvPoint3D() + expandedVector).ToCameraSpacePoint();
+                joints[bone.Item2] = joint2;
+            }
+        }
+
+        private void RemoveHalfBody(Dictionary<JointType, Joint> joints, List<JointType> removeJoints)
+        {
+
         }
     }
 }
