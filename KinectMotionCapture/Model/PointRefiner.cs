e0ab264bdfd78ed21b1e3bbd2fb76e950e1c6b9b
non <mail@non117.com>
2015-03-26 14:43:01 +0900

    ちょっとできた
    工事の音がうるさい

--- KinectMotionCapture/Model/PointRefiner.cs
+++ KinectMotionCapture/Model/PointRefiner.cs
@@ -9,13 +9,62 @@ using OpenCvSharp;
 
 namespace KinectMotionCapture
 {
+    struct PointAndColor
+    {
+        public CvPoint3D64f point;
+        public CvColor color;
+        public JointType nearestJoint;
+        public double distance;
+        public PointAndColor(CvPoint3D64f point, CvColor color, JointType nearest, double distance)
+        {
+            this.point = point;
+            this.color = color;
+            this.nearestJoint = nearest;
+            this.distance = distance;
+        }
+    }
+
     class PointRefiner
     {
         Dictionary<JointType, CvPoint3D64f> standardJoints;
+        Dictionary<JointType, List<PointAndColor>> pointColorStore;
+        StandardSkeleton standardSkeleton;
         // 骨と表面情報を受け取って、標準骨格のまわりにマッピングしていく。
-        // 基準となる標準骨格はとりあえず、最初のフレームの最初のレコード
         // とりあえず胴体近傍を出力することが目標
-        // 最近傍を計算するアレ. ヒューリスティックを導入するか、全探索か。
-        // 可視化を急ぐ
+        /// <summary>
+        /// こんすとらくたん
+        /// 標準骨格を与えられた骨格と骨の長さ情報をもとに作っておく
+        /// </summary>
+        /// <param name="skltn"></param>
+        /// <param name="standardPose"></param>
+        public PointRefiner(StandardSkeleton skltn, Dictionary<JointType, CvPoint3D64f> standardPose)
+        {
+            this.standardSkeleton = skltn;
+            this.standardJoints = this.standardSkeleton.FixBoneLength(standardPose);
+            this.pointColorStore = new Dictionary<JointType, List<PointAndColor>>();
+            foreach (JointType key in this.pointColorStore.Keys)
+            {
+                this.pointColorStore[key] = new List<PointAndColor>();
+            }
+        }
+
+        /// <summary>
+        /// 最近傍の関節を見つけてデータを貯める
+        /// </summary>
+        /// <param name="rawData"></param>
+        public void AddData(List<Tuple<CvPoint3D64f, CvColor>> rawData)
+        {
+            foreach (var pointColorPair in rawData)
+            {
+                PointAndColor pac = this.ConvertToPointAndColor(pointColorPair);
+                this.pointColorStore[pac.nearestJoint].Add(pac);
+            }
+        }
+
+        public PointAndColor ConvertToPointAndColor(Tuple<CvPoint3D64f, CvColor> pointColorPair)
+        {
+            
+            
+        }
     }
 }
