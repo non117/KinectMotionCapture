1a2fba1f26e1d4f9901b5ec94bd8365c7aa83e0a
non <mail@non117.com>
2015-09-27 20:01:05 +0900

    Settingsクラスを追加

---
+++ KinectMotionCapture/Model/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectMotionCapture
+{
+    [Serializable]
+    public class Settings
+    {
+        public string[] dataDirectories
+        {
+            get;
+            set;
+        }
+        public string[] mapFiles
+        {
+            get;
+            set;
+        }
+        public string[] cameraFiles
+        {
+            get;
+            set;
+        }
+    }
+}
